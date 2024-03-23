3b7285fd3c999f7e97fd76d097ce66d46f26e19f
Metoděj Janota <janotametodej@gmail.com>
2024-03-23 18:50:45 +0100

    Start

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import ThemeSwitch from "@/components/util/themeSwitch";
+
+export default function Home() {
+  return (
+    <>
+      <ThemeSwitch />
+      <Button>d</Button>
+    </>
+  );
+}
